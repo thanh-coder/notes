b57e98f5f82989cf1d276a4552c6ff1dbdc85d13
codertokyo <tranhung.ptit>
2018-10-01 23:39:16 +0700

    insert data into database

---
+++ models-mongoose/notes.js
@@ -0,0 +1,92 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var dburl = undefined;
+exports.connect = function(thedburl, callback) {
+    dburl = thedburl;
+    mongoose.connect(dburl);
+}
+ 
+exports.disconnect = function(callback) {
+    mongoose.disconnect(callback);
+}
+ 
+var NoteSchema = new Schema({
+    notekey: String,
+    title: String,
+    body: String
+});
+ 
+mongoose.model('Note', NoteSchema);
+var Note = mongoose.model('Note');
+ 
+exports.create = function(key, title, body, callback) {
+    var newNote = new Note();
+    newNote.notekey = key;
+    newNote.title = title;
+    newNote.body = body;
+    newNote.save(function(err) {
+        if(err)
+            callback(err);
+        else
+            callback();
+    });
+}
+ 
+exports.update = function(key, title, body, callback) { 
+    exports.read(key, function(err, doc) {    
+        if(err)
+            callback(err);
+        else { 
+            doc.notekey = key;
+            doc.title = title;
+            doc.body = body;
+            doc.save(function(err) {
+                if(err)
+                    callback(err);
+                else
+                    callback();
+            });
+        }
+    });
+}
+ 
+exports.read = function(key, callback) {
+    Note.findOne({ notekey: key }, function(err, doc) {
+        if(err) 
+            callback(err);
+        else
+            callback(null, doc);
+    });
+}
+ 
+exports.destroy = function(key, callback) {
+    exports.read(key, function(err, doc) {
+        if(err)
+            callback(err);
+        else {
+            doc.remove();
+            callback();
+        }
+    });
+}
+ 
+exports.titles = function(callback) {
+    Note.find().exec(function(err, docs) {
+        if(err)
+            callback(err);
+        else {
+            if(docs) {
+                var noteList = [];
+                docs.forEach(function(note) {
+                    noteList.push({
+                    key: note.notekey,
+                    title: note.title 
+                    }); 
+                });
+                callback(null, noteList);
+            } else { 
+                callback();
+            }
+        }
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,28 @@ var notes = undefined;
 exports.configure = function(params) {
     notes = params;
 }
- 
-exports.index = router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Notes', notes: notes });
+ var express = require('express');
+var router = express.Router();
+var notes = undefined;
+exports.configure = function(params) {
+    notes = params;
+}
+/* GET home page. */
+  
+exports.index = router.get('/', function(req, res, next) {  
+    notes.titles(function(err, titles) {        
+        if(err) {           
+            res.render('showerror', {
+                title: "Could not retrieve note keys from data store",
+                error: err,
+                user: req.user ? req.user : undefined
+            });
+        } else {                        
+            res.render('index', { 
+                title: "Notes", 
+                notes: titles,
+                user: req.user ? req.user : undefined
+            });         
+        }
+    });
 });
